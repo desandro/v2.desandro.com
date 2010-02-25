ebf8a79c98604ceb82130317243c3cfb7f33dd76
desandro <david@desandro.com>
2010-02-25 07:31:25 -0500

    Remove work folded

--- work/txp_org_v3/txporgv3_05/includes/templates/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="site_footer">
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-</div> <!-- /#site_footer -->
--- work/txp_org_v3/txporgv3_05/includes/templates/html_head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" href="css/yui_reset.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="css/txporgv3_04.css" type="text/css" media="screen" />
-	
-	<script type="text/javascript" src="js/jquery-1.3.2.min.js" charset="utf-8"></script>
-	<script type="text/javascript" src="js/txporgv3.js" charset="utf-8"></script>
