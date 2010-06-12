44926a5a09b4bb880c72efc51117e43b16db3b7f
desandro <david@desandro.com>
2010-06-12 12:13:56 -0400

    Fight Flash of Un-styled Type with Web Font loading styles

--- _base/js/application.js
+++ _base/js/application.js
@@ -1,3 +1,7 @@
+// Paul Irish FOUC fighter
+// http://paulirish.com/2009/avoiding-the-fouc-v3/
+(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement);
+
 ;$(function(){
     
     $('.short-url input').click(function(){
