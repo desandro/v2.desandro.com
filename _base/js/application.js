941a56ef93f4bca18dc3bec7aa7a8f9d08dfca38
David DeSandro <david@desandro.com>
2010-11-05 06:57:39 -0400

    Add Modernizr, move scripts to bottom, add closing template

--- _base/js/application.js
+++ _base/js/application.js
@@ -1,10 +1,4 @@
-// Paul Irish FOUC fighter
-// http://paulirish.com/2009/avoiding-the-fouc-v3/
-(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement);
-
-try{Typekit.load();}catch(e){}
-
-!window.jQuery && document.write('<script src="/_base/js/jquery-1.4.2.min.js"><\/script>')
+!window.jQuery && document.write('<script src="/_base/js/jquery-1.4.2.min.js"><\/script>');
 
 $(function(){
   $('.short-url input').click(function(){
